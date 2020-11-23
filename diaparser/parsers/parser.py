a9bb1936491edf0eabafb8c4553b7770c1e5745b
Giuseppe Attardi <attardi@gmail.com>
2020-11-23 20:18:45 +0100

    Fix to predict invocation.

--- diaparser/parsers/parser.py
+++ diaparser/parsers/parser.py
@@ -151,8 +151,8 @@ class Parser():
         self.transform.eval()
         if args.prob:
             self.transform.append(Field('probs'))
-        if args.lang:
-            self.transform.reader = Tokenizer(args.lang, dir=args.cache_dir).reader()
+        if args.text:
+            self.transform.reader = Tokenizer(args.text, dir=args.cache_dir).reader()
 
         logger.info("Loading the data")
         dataset = Dataset(self.transform, data)
