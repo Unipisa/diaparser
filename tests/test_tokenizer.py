d9f84aae442ac7a588e7f6f4eb8e725056c9a386
attardi <giuseppe.attardi@garr.it>
2021-03-18 17:21:18 +0100

    Pass lang arg.

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -17,21 +17,22 @@ class TestTokenizer(unittest.TestCase):
         }
         
     def test_download_resources(self):
-        tokenizer = Tokenizer(**self.args)
+        tokenizer = Tokenizer(self.args['lang'])
         
         self.assertTrue(os.path.isdir(self.MODEL_DIR))
         self.assertTrue(os.path.exists(os.path.join(self.MODEL_DIR, 'tokenizer', self.args['lang'])))
         self.assertTrue(os.path.exists(os.path.join(self.MODEL_DIR, 'tokenizer', self.args['lang'], 'tokenize')))
     
     def test_tokenize(self):
-        tokenizer = Tokenizer(**self.args)
+        tokenizer = Tokenizer(self.args['lang'])
         sentences = tokenizer.predict('Ha chiamato il dr. Rossi.Vuole salutarti.')
         self.assertEqual(len(sentences), 2)
 
     def test_corpus_load(self):
-        tokenizer = Tokenizer(**self.args)
+        tokenizer = Tokenizer(self.args['lang'])
         sin = io.StringIO("Un corazziere contro Scalfaro. L'attore le disse baciami o torno a riprendermelo.")
         
         for line in tokenizer.format(tokenizer.predict(sin.read())):
             if line and not line.startswith('#'):
+                # CoNLL-U format has 10 tsv:
                 assert len(line.split('\t')) == 10, line
